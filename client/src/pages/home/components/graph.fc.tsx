fca9bd96863df3d2a21b82ebbb9f0ebcf1f23c1c
Liran-tal <taliran44@gmail.com>
2022-04-13 08:59:10 +0300

    add util functions for chart points labels

--- client/src/pages/home/components/graph.fc.tsx
+++ client/src/pages/home/components/graph.fc.tsx
@@ -18,9 +18,24 @@ const GraphFC: React.FC<I_Props> = ({ cpuUsage }) => {
 
 const getGraphLabels = (cpuUsage: I_DataPoint[]): string[] => {
   return cpuUsage.map((dataPoint) => {
-    const time = (date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+    const date = getDatePortion(dataPoint.Timestamp);
+    const time = getTimePortion(dataPoint.Timestamp);
+    return `${date} ${time}`;
   })
 }
 
+const getDatePortion = (date: Date): string => {
+  const day = date.getDay();
+  const month = date.getDate();
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
+const getTimePortion = (date: Date): string => {
+  return date.toLocaleTimeString('en-GB', { 
+    hour: '2-digit', 
+    minute: '2-digit',
+  })
+}
 
 export default GraphFC
